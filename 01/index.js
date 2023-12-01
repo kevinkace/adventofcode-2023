125aecd3e544712a873665091392a0fe230e9e6b
Kevin Cameron <kevinacameron@hotmail.com>
2023-11-30 23:12:53 -0800

    01.1

--- 01/index.js
+++ 01/index.js
@@ -1,3 +1,32 @@
+
+import { readFile }      from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import path              from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = path.dirname(__filename);
+
+const input = await readFile(path.join(__dirname, "input.txt"), 'utf8');
+
+
+export function getNums(row) {
+    const nums = row.match(/\d/g);
+
+    return [ nums[0], nums.at(-1) ];
+}
+
+export function parseNums([ n0, n1 ]) {
+    return parseInt(`${n0}${n1}`);
+}
+
 export function day01() {
-    console.log('day01');
+    let sum = 0;
+
+    input.split("\n").forEach((line) => {
+        const num =  parseNums(getNums(line));
+
+        sum += num;
+    });
+
+    return sum;
 }
--- test/day01.spec.js
+++ test/day01.spec.js
@@ -1,8 +1,24 @@
 import { expect, test, describe } from 'vitest'
-import { day01 } from "../01";
+import { day01, getNums, parseNums } from "../01";
 
 describe("day01", () => {
     test("is a fn", () => {
         expect(typeof day01).toBe("function");
+    });
+
+    test("returns the correct value", () => {
+        const result = day01();
+
+        console.log(result);
+        expect(typeof result).toBe("number");
+    });
+
+    test("gets nums from row", () => {
+        expect(getNums("abc123")).toEqual(["1", "3"]);
+        expect(getNums("abc1")).toEqual(["1", "1"]);
+    })
+
+    test("parses nums", () => {
+        expect(parseNums([ "1", "3" ])).toEqual(13);
     });
 });
