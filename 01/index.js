3d6b5cab89a3c96f0b0d890a6a9650a727b59c48
Kevin Cameron <kevinacameron@hotmail.com>
2023-11-30 20:02:50 -0800

    start

---
+++ 01/index.js
@@ -0,0 +1,3 @@
+export function day01() {
+    console.log('day01');
+}
---
+++ test/day01.spec.js
@@ -0,0 +1,7 @@
+import { day01 } from "../01";
+
+describe("day01", () => {
+    test("is a fn", () => {
+        expect(typeof day01).toBe("function");
+    });
+});
