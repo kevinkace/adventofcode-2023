d2d46138f909f4de9409be22c31af6bc5c722a6f
Kevin Cameron <kevinacameron@hotmail.com>
2023-12-02 21:02:34 -0800

    cleanup day 1

--- days/day01.js
+++ days/day01.js
@@ -1,26 +1,18 @@
 const numMap = {
-    one   : 1,
-    two   : 2,
-    three : 3,
-    four  : 4,
-    five  : 5,
-    six   : 6,
-    seven : 7,
-    eight : 8,
-    nine  : 9,
-};
-
-const numMap2 = {
-    one : "o1e",
-    two : "t2o",
+    one   : "o1e",
+    two   : "t2o",
     three : "t3e",
-    four : "f4r",
-    five : "f5e",
-    six : "s6x",
+    four  : "f4r",
+    five  : "f5e",
+    six   : "s6x",
     seven : "s7n",
     eight : "e8t",
-    nine : "n9e",
+    nine  : "n9e",
 };
+
+export function parse(str) {
+    return str.split("\n");
+}
 
 /**
  * Get first and last digit in string, and concat to a 2 digit number
@@ -40,88 +32,16 @@ export function getNum(row) {
  * @param {string} input - eg `"abc123\nabc456"`
  * @returns {number} - eg `59`, because `13 + 46 = 59`
  */
-export function sumRows(input, replacer) {
-    let sum = 0;
+export function addRowToSum(sum, row) {
+    const num =  getNum(row);
 
-    input.split("\n").forEach((line) => {
-        if (replacer) {
-            line = replacer(line);
-        }
-
-        const num =  getNum(line);
-
-        sum += num;
-    });
-
-    return sum;
+    return sum + num;
 }
 
-/**
- * Get all matches in string
- * @param {string} str - eg `"one2eighthree"`
- * @returns {object|false} - eg `{ 0 : "one", 4 : "eight", 9 : "three" }`
- */
-export function getMatches(str) {
-    const matches = {};
-
-    let found = false;
-
+export function replaceStringsWithNumbers(str) {
     Object.entries(numMap).forEach(([ word, num ]) => {
-        const match = str.indexOf(word);
-
-        if (match > -1) {
-            matches[match] = word;
-            found          = true;
-        }
-    });
-
-    return found && matches;
-}
-
-/**
- * Swap lowest match in string
- * @param {string} str - eg `"one2eighthree"`
- * @param {object} matches - eg `{ 0 : "one", 4 : "eight", 9 : "three" }`
- * @returns {string} - eg `"12eighthree"`
- */
-export function swapLowestMatch(str, matches) {
-    const lowestMatch = Math.min(...Object.keys(matches));
-
-    const word = matches[lowestMatch];
-
-    return str.replace(word, numMap[word]);
-}
-
-/**
- * Swap all matches in string
- * @param {string} str - eg `"one2eighthree"`
- * @returns {string} - eg `"128hree"`
- */
-export function swapAllMatches(str) {
-    const matches = getMatches(str);
-
-    if (!matches) {
-        return str;
-    }
-
-    const swapped = swapLowestMatch(str, matches);
-
-    return swapAllMatches(swapped);
-}
-
-export function swapAllMatches2(str) {
-    Object.entries(numMap2).forEach(([ word, num ]) => {
         str = str.replaceAll(word, num);
     });
 
     return str;
-}
-
-/**
- * Swap all matches in all rows
- * @param {string} str - eg `"one2eighthree\nfour5fivesix"`
- * @returns {string} - eg `"128hree\n4556"
- */
-export function swapAndSum(str) {
-    return sumRows(str, swapAllMatches2);
 }
--- test/01/day01.spec.js
+++ test/01/day01.spec.js
@@ -1,6 +1,6 @@
-import { readFile, writeFile } from "node:fs/promises";
-import { fileURLToPath }       from "node:url";
-import path                    from "node:path";
+import { readFile }      from "node:fs/promises";
+import { fileURLToPath } from "node:url";
+import path              from "node:path";
 
 import { expect, test, describe } from "vitest";
 
@@ -8,98 +8,47 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
 
 const input    = await readFile(path.join(__dirname, "input.txt"), "utf8");
-const output   = await readFile(path.join(__dirname, "output.txt"), "utf8");
 const sample01 = await readFile(path.join(__dirname, "sample01.txt"), "utf8");
 const sample02 = await readFile(path.join(__dirname, "sample02.txt"), "utf8");
 
-import { getNum, swapAndSum, getMatches, swapLowestMatch, swapAllMatches, sumRows, swapAllMatches2 } from "../../days/day01.js";
+import { getNum, replaceStringsWithNumbers, addRowToSum, parse, addRowToSum } from "../../days/day01.js";
 
 describe("day 01", () => {
     test("gets nums from row", () => {
-        expect(getNum("abc123")).toEqual([ "1", "3" ]);
-        expect(getNum("abc1")).toEqual([ "1", "1" ]);
+        expect(getNum("abc123")).toEqual(13);
+        expect(getNum("abc1")).toEqual(11);
+        expect(getNum("abc")).toEqual(0);
+        expect(getNum("threeight")).toEqual(0);
     });
 
-    test("can num swap", () => {
-        expect(swapAndSum("one")).toBe("1");
-        expect(swapAndSum("oneone")).toBe("11");
-        expect(swapAndSum("one1one\ntwo")).toBe("111\n2");
-        // expect(numSwap("eightwothree")).toBe("8wo3");
+    test("can sum rows", () => {
+        expect(addRowToSum(0, "abc123")).toEqual(13);
+        expect(addRowToSum(13, "abc456")).toEqual(59);
     });
 
-    test.skip("can get matches", () => {
-        expect(getMatches("one")).toMatchObject({ 0 : "one" });
-        expect(getMatches("oneone")).toMatchObject({ 0 : "one" });
-        expect(getMatches("nine1one")).toMatchObject({ 0 : "nine", 5 : "one" });
-        expect(getMatches("oneeight")).toMatchObject({ 0 : "one", 3 : "eight" });
-        expect(getMatches("oneight")).toMatchObject({ 0 : "one", 2 : "eight" });
-        expect(getMatches("asdf")).toBe(false);
-        expect(getMatches("eightwothree")).toMatchObject({ 0 : "eight", 4 : "two", 7 : "three" });
+    test("answer part 1", () => {
+        let result = parse(sample01)
+            .reduce(addRowToSum, 0);
+
+        expect(result).toEqual(142);
+
+        result = parse(input)
+            .reduce(addRowToSum, 0);
+
+        expect(result).toMatchSnapshot();
     });
 
-    test.skip("swap lowest", () => {
-        expect(swapLowestMatch("one", { 0 : "one" })).toBe("1");
-        expect(swapLowestMatch("oneone", { 0 : "one" })).toBe("1one");
-        expect(swapLowestMatch("nine1one", { 0 : "nine", 5 : "one" })).toBe("91one");
-        expect(swapLowestMatch("oneeight", { 0 : "one", 3 : "eight" })).toBe("1eight");
-        expect(swapLowestMatch("oneight", { 0 : "one", 2 : "eight" })).toBe("1ight");
-        expect(swapLowestMatch("eightwothree", { 0 : "eight", 4 : "two", 7 : "three" })).toBe("8wothree");
-    });
+    test("answer part 2", async () => {
+        let result = parse(sample02)
+            .map(replaceStringsWithNumbers)
+            .reduce(addRowToSum, 0);
 
-    test("can swap all", () => {
-        Object.entries({
-            "threeight" : "t3e8t",
-            "fiveight"  : "f5e8t",
-            "sevenine"  : "s7n9e",
+        expect(result).toEqual(281);
 
-            // base compounds
-            "oneight"   : "1ight",
-            "threeight" : "3ight",
-            "fiveight"  : "5ight",
-            "sevenine"  : "7ine",
-            "eightwo"   : "8wo",
-            "eighthree" : "8hree",
-            "nineight"  : "9ight"
-        }).forEach(([ input, expected ]) => {
-            expect(swapAllMatches(input)).toBe(expected);
-        });
-    });
+        result = parse(input)
+            .map(replaceStringsWithNumbers)
+            .reduce(addRowToSum, 0);
 
-    test("non-swapping", () => {
-        [{
-            input,
-        }, {
-            input : output
-        }, {
-            input    : sample01,
-            expected : 142
-        }].forEach(({ input : _input, expected }) => {
-            const result = sumRows(_input);
-
-            console.log({ result });
-
-            expect(typeof result).toBe("number");
-
-            if (expected) {
-                expect(result).toBe(expected);
-            }
-        });
-    });
-
-    test.only("swapping", async () => {
-        let result = swapAndSum(input);
-
-        console.log({ result });
-
-        expect(typeof result).toBe("number");
-        expect(result).toBeGreaterThan(54095);
-
-
-        result = swapAndSum(sample02);
-
-        console.log({ result });
-
-        expect(typeof result).toBe("number");
-        expect(result).toBe(281);
+        expect(result).toMatchSnapshot();
     });
 });
