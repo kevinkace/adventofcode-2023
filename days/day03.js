0a881a8b8ae88113ce58429cf621144d8a5f9a8b
Kevin Cameron <kevinacameron@hotmail.com>
2023-12-03 10:58:05 -0800

    d03p1

---
+++ days/day03.js
@@ -0,0 +1,99 @@
+export class Regions {
+    constructor(str) {
+        this.str = str;
+        this.parsed = str.split(/\r\n|\n/);
+        this.regions = [];
+        this.specials = [];
+
+        this.getRegions();
+    }
+
+    getRegions() {
+        this.parsed.forEach((line, y) => {
+            for (let x = 0; x < line.length; x++) {
+                const currChar = line[x];
+
+                if (/\d/.test(currChar)) {
+                    this.addToRegion(currChar, x, y);
+
+                    continue
+                }
+
+                this.closeRegion();
+
+                if (currChar === ".") {
+                    continue;
+                }
+
+                if (['$', '*', '/', '-', '#', '@', '&', '%', '=', '+'].includes(currChar)) {
+                    this.specials.push({ char : currChar, x, y });
+                }
+            }
+
+            this.closeRegion();
+        });
+    }
+
+    addToRegion(number, x, y) {
+        if (!this.currRegion) {
+            this.currRegion = { number, x1 : x, y };
+
+            return;
+        }
+
+        this.currRegion = {
+            number : `${this.currRegion.number}${number}`,
+            x1     : this.currRegion.x1,
+            x2     : x,
+            y      : y || this.currRegion.y
+        }
+    }
+
+    closeRegion() {
+        if (!this.currRegion) {
+            return;
+        }
+
+        if (!this.currRegion.x2) {
+            this.currRegion.x2 = this.currRegion.x1;
+        }
+
+        this.currRegion.number = parseInt(this.currRegion.number, 10);
+
+        this.regions.push(this.currRegion);
+
+        delete this.currRegion;
+    }
+
+    getCollisions() {
+        return this.regions.filter((region, i) => {
+            return this.specials.some((special, j) => {
+                return specialCollision(region, special);
+            });
+        });
+    }
+
+    sumCollisions() {
+        return this.getCollisions().reduce((acc, curr) => acc + curr.number, 0);
+    }
+
+}
+
+export function specialCollision(region, special) {
+    const x1 = region.x1 - 1;
+    const x2 = region.x2 + 1;
+    const y1 = region.y - 1;
+    const y2 = region.y + 1;
+
+    const collision = special.x >= x1 && special.x <= x2 && special.y >= y1 && special.y <= y2;
+
+    if (!collision) {
+        return false;
+    }
+
+    return true;
+}
+
+export function testCollision(box1, box2) {
+    return box1.x1 < box2.x2 && box1.x2 > box2.x1 && box1.y1 < box2.y2 && box1.y2 > box2.y1;
+}
---
+++ test/03/day03.spec.js
@@ -0,0 +1,89 @@
+import { readFile, writeFile } from "node:fs/promises";
+import { fileURLToPath }       from "node:url";
+import path                    from "node:path";
+
+import { expect, test, describe } from "vitest";
+
+import { parse } from "../../days/day02.js";
+
+import typedefs from "../../typedefs.js";
+
+import { Regions, sumIntegersAdjacentToSymbols, testCollision } from "../../days/day03.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname  = path.dirname(__filename);
+
+const input    = await readFile(path.join(__dirname, "input.txt"), "utf8");
+const sample01 = await readFile(path.join(__dirname, "sample01.txt"), "utf8");
+const sample01Small = await readFile(path.join(__dirname, "sample01-small.txt"), "utf8");
+const inputSmall = await readFile(path.join(__dirname, "input-small.txt"), "utf8");
+
+import { samples } from "./samples.js";
+
+
+describe("day 03", () => {
+    // test("can parse input", () => {
+    //     expect(parse(sample01)).toMatchSnapshot();
+    // });
+
+    test("can detect collisions", () => {
+        expect(testCollision(
+            { x1: 1, y1: 1, x2: 4, y2: 4 },
+            { x1: 3, y1: 3, x2: 6, y2: 6 }
+        )).toBe(true);
+        expect(testCollision(
+            { x1: 1, y1: 1, x2: 2, y2: 2 },
+            { x1: 3, y1: 3, x2: 6, y2: 6 }
+        )).toBe(false);
+    });
+
+    test("can get numbers", () => {
+        const regions = new Regions(sample01);
+
+        expect(regions).toMatchSnapshot();
+    });
+
+    test("gets collisions", () => {
+        const regions = new Regions(sample01);
+
+        expect(regions.getCollisions()).toMatchSnapshot();
+    });
+
+    test("answer pt 1", () => {
+        let regions = new Regions(sample01);
+
+        expect(regions.sumCollisions()).toEqual(4361);
+
+        regions = new Regions(input);
+        // const collisions = regions.getCollisions();
+        const sum = regions.sumCollisions();
+        // console.log("my sum: " + sum);
+        // console.log(regions.regions.slice(80));
+        expect(sum).toBeLessThan(3012388);
+        expect(sum).toBeGreaterThan(43211);
+        expect(sum).toBeGreaterThan(553499);
+        expect(sum === 553585).toBe(false);
+        expect(sum).toMatchSnapshot();
+    });
+
+    test.skip("med input", () => {
+        let regions = new Regions(inputSmall);
+        expect(regions.regions).toMatchSnapshot();
+        expect(regions.specials).toMatchSnapshot();
+        const collisions = regions.getCollisions();
+
+        const sum = collisions.reduce((acc, curr) => {
+            return acc + curr.number;
+        }, 0);
+
+        console.log(sum);
+    });
+
+    test.skip("samples", () => {
+        samples.forEach(sample => {
+            const regions = new Regions(sample);
+
+            console.log(regions.sumCollisions());
+        });
+    });
+});
