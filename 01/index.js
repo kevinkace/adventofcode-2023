2823bf6a3677f439f4146aab6df5b57c66764c4d
Kevin Cameron <kevinacameron@hotmail.com>
2023-12-02 16:39:08 -0800

    cleanup

--- 01/index.js
+++ 01/index.js
@@ -1,35 +1,42 @@
 const numMap = {
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
-    "nine": 9,
-}
+    one   : 1,
+    two   : 2,
+    three : 3,
+    four  : 4,
+    five  : 5,
+    six   : 6,
+    seven : 7,
+    eight : 8,
+    nine  : 9,
+};
 
-
-export function getNums(row) {
+/**
+ * Get first and last digit in string, and concat to a 2 digit number
+ * @param {string} row - eg `"abc123"`
+ * @returns {number} - eg `13`
+ */
+export function getNum(row) {
     const nums = row.match(/\d/g);
 
-    if (!nums) {
-        return ["0", "0"];
-    }
-
-    return [ nums[0], nums.at(-1) ];
+    return nums ?
+        parseInt(`${nums.at(0)}${nums.at(-1)}`, 10) :
+        0;
 }
 
-export function parseNums([ n0, n1 ]) {
-    return parseInt(`${n0}${n1}`);
-}
-
-export function sumFromRows(input) {
+/**
+ * Sum all numbers in input
+ * @param {string} input - eg `"abc123\nabc456"`
+ * @returns {number} - eg `59`, because `13 + 46 = 59`
+ */
+export function sumRows(input, replacer) {
     let sum = 0;
 
     input.split("\n").forEach((line) => {
-        const num =  parseNums(getNums(line));
+        if (replacer) {
+            line = replacer(line);
+        }
+
+        const num =  getNum(line);
 
         sum += num;
     });
@@ -37,12 +44,13 @@ export function sumFromRows(input) {
     return sum;
 }
 
+/**
+ * Get all matches in string
+ * @param {string} str - eg `"one2eighthree"`
+ * @returns {object|false} - eg `{ 0 : "one", 4 : "eight", 9 : "three" }`
+ */
 export function getMatches(str) {
-    const matches = {
-        // idx : word eg
-        // 0: "one",
-        // 4: "three"
-    };
+    const matches = {};
 
     let found = false;
 
@@ -51,13 +59,19 @@ export function getMatches(str) {
 
         if (match > -1) {
             matches[match] = word;
-            found = true;
+            found          = true;
         }
     });
 
     return found && matches;
 }
 
+/**
+ * Swap lowest match in string
+ * @param {string} str - eg `"one2eighthree"`
+ * @param {object} matches - eg `{ 0 : "one", 4 : "eight", 9 : "three" }`
+ * @returns {string} - eg `"12eighthree"`
+ */
 export function swapLowestMatch(str, matches) {
     const lowestMatch = Math.min(...Object.keys(matches));
 
@@ -66,6 +80,11 @@ export function swapLowestMatch(str, matches) {
     return str.replace(word, numMap[word]);
 }
 
+/**
+ * Swap all matches in string
+ * @param {string} str - eg `"one2eighthree"`
+ * @returns {string} - eg `"128hree"`
+ */
 export function swapAllMatches(str) {
     const matches = getMatches(str);
 
@@ -78,12 +97,11 @@ export function swapAllMatches(str) {
     return swapAllMatches(swapped);
 }
 
-export function swapMatchesAllRows(str) {
-    let newInput = str;
-
-    Object.entries(numMap).forEach(([ word, num ]) => {
-        newInput = swapAllMatches(newInput);
-    });
-
-    return newInput;
+/**
+ * Swap all matches in all rows
+ * @param {string} str - eg `"one2eighthree\nfour5fivesix"`
+ * @returns {string} - eg `"128hree\n4556"
+ */
+export function swapAndSum(str) {
+    return sumRows(str, swapAllMatches);
 }
--- test/01/day01.spec.js
+++ test/01/day01.spec.js
@@ -1,34 +1,29 @@
-import { readFile, writeFile }      from 'node:fs/promises';
-import { fileURLToPath } from 'node:url';
-import path              from 'node:path';
+import { readFile, writeFile } from "node:fs/promises";
+import { fileURLToPath }       from "node:url";
+import path                    from "node:path";
 
-import { expect, test, describe } from 'vitest'
+import { expect, test, describe } from "vitest";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname  = path.dirname(__filename);
 
-const input    = await readFile(path.join(__dirname, "input.txt"), 'utf8');
-const output   = await readFile(path.join(__dirname, "output.txt"), 'utf8');
-const sample01 = await readFile(path.join(__dirname, "sample01.txt"), 'utf8');
-const sample02 = await readFile(path.join(__dirname, "sample02.txt"), 'utf8');
+const input    = await readFile(path.join(__dirname, "input.txt"), "utf8");
+const output   = await readFile(path.join(__dirname, "output.txt"), "utf8");
+const sample01 = await readFile(path.join(__dirname, "sample01.txt"), "utf8");
+const sample02 = await readFile(path.join(__dirname, "sample02.txt"), "utf8");
 
-import { getNums, parseNums, swapMatchesAllRows, getMatches, swapLowestMatch, swapAllMatches, sumFromRows } from "../../01";
+import { getNum, swapAndSum, getMatches, swapLowestMatch, swapAllMatches, sumRows } from "../../01";
 
 describe("day 01", () => {
-
     test("gets nums from row", () => {
-        expect(getNums("abc123")).toEqual(["1", "3"]);
-        expect(getNums("abc1")).toEqual(["1", "1"]);
-    })
-
-    test("parses nums", () => {
-        expect(parseNums([ "1", "3" ])).toEqual(13);
+        expect(getNum("abc123")).toEqual([ "1", "3" ]);
+        expect(getNum("abc1")).toEqual([ "1", "1" ]);
     });
 
     test("can num swap", () => {
-        expect(swapMatchesAllRows("one")).toBe("1");
-        expect(swapMatchesAllRows("oneone")).toBe("11");
-        expect(swapMatchesAllRows("one1one\ntwo")).toBe("111\n2");
+        expect(swapAndSum("one")).toBe("1");
+        expect(swapAndSum("oneone")).toBe("11");
+        expect(swapAndSum("one1one\ntwo")).toBe("111\n2");
         // expect(numSwap("eightwothree")).toBe("8wo3");
     });
 
@@ -51,45 +46,55 @@ describe("day 01", () => {
         expect(swapLowestMatch("eightwothree", { 0 : "eight", 4 : "two", 7 : "three" })).toBe("8wothree");
     });
 
-    test("can swap all", () => {
-
+    test.only("can swap all", () => {
         Object.entries({
-            "one" : "1",
-            "oneone" : "11",
-            "eightwothree" : "8wo3",
-            "abcone2threexyz" : "abc123xyz",
-            "xtwone3four" : "x2ne34",
-            "4nineeightseven2" : "49872",
-            "zoneight234" : "z1ight234",
-            "7pqrstsixteen" : "7pqrst6teen",
-            "oneoneone" : "111",
+            // base compounds
+            "oneight" : "1ight",
+            "threeight" : "3ight",
+            "fiveight" : "5ight",
+            "sevenine" : "7ine",
+            "eightwo" : "8wo",
+            "eighthree" : "8hree",
+            "nineight" : "9ight"
 
-            "four82nine74" : "482974",
-            "hlpqrdh3" : "hlpqrdh3",
-            "eightsevenhrsseven988" : "87hrs7988",
-            "324pzonenine" : "324pz19",
-            "fglpbone79fourvrgcmgklbmthree" : "fglpb1794vrgcmgklbm3",
-            "fmbbkvthdcdmcjxzclk42six4" : "fmbbkvthdcdmcjxzclk4264",
-            "four22xcqsnvktnpfshtmm" : "422xcqsnvktnpfshtmm",
-            "qmfsccxsixfivelnmpjqjcsc1sixpfpmeight" : "qmfsccx65lnmpjqjcsc16pfpm8",
-            "eight1nine5nine9six" : "8195996",
-            "s4r91seven" : "s4r917",
-            "6pspkslrnxpplkhgqlcqfour" : "6pspkslrnxpplkhgqlcq4",
-            "sixeightnzrzgjvsrnmtqgx5" : "68nzrzgjvsrnmtqgx5",
-            "sixtwo1" : "621",
-            "h6" : "h6",
-            "five8pbcsllrbvg787" : "58pbcsllrbvg787",
-            "dpfhfeight28onefourtwo" : "dpfhf828142",
-            "vxqbtkxjtwoz3seven" : "vxqbtkxj2z37",
-            "8ksrcjrcmpbq9rtvtvrbgljzqvbnxddnzt" : "8ksrcjrcmpbq9rtvtvrbgljzqvbnxddnzt",
-            "mpftpsgp6fourvdmltwojd9" : "mpftpsgp64vdml2jd9",
-            "2fivetwosix" : "2526",
-            "3qqx2" : "3qqx2",
-            "jsbdh16snnllpvvgnggfive5nhjpgdzh" : "jsbdh16snnllpvvgngg55nhjpgdzh",
-            "4fmgmmbonegtsnqfdqt1pm" : "4fmgmmb1gtsnqfdqt1pm",
-            "threeighthree" : "3igh3",
-            "four1six" : "416",
-            "sdsevenine" : "sd7ine"
+
+
+            // one                : "1",
+            // oneone             : "11",
+            // eightwothree       : "8wo3",
+            // abcone2threexyz    : "abc123xyz",
+            // xtwone3four        : "x2ne34",
+            // "4nineeightseven2" : "49872",
+            // zoneight234        : "z1ight234",
+            // "7pqrstsixteen"    : "7pqrst6teen",
+            // oneoneone          : "111",
+
+            // four82nine74                          : "482974",
+            // hlpqrdh3                              : "hlpqrdh3",
+            // eightsevenhrsseven988                 : "87hrs7988",
+            // "324pzonenine"                        : "324pz19",
+            // fglpbone79fourvrgcmgklbmthree         : "fglpb1794vrgcmgklbm3",
+            // fmbbkvthdcdmcjxzclk42six4             : "fmbbkvthdcdmcjxzclk4264",
+            // four22xcqsnvktnpfshtmm                : "422xcqsnvktnpfshtmm",
+            // qmfsccxsixfivelnmpjqjcsc1sixpfpmeight : "qmfsccx65lnmpjqjcsc16pfpm8",
+            // eight1nine5nine9six                   : "8195996",
+            // s4r91seven                            : "s4r917",
+            // "6pspkslrnxpplkhgqlcqfour"            : "6pspkslrnxpplkhgqlcq4",
+            // sixeightnzrzgjvsrnmtqgx5              : "68nzrzgjvsrnmtqgx5",
+            // sixtwo1                               : "621",
+            // h6                                    : "h6",
+            // five8pbcsllrbvg787                    : "58pbcsllrbvg787",
+            // dpfhfeight28onefourtwo                : "dpfhf828142",
+            // vxqbtkxjtwoz3seven                    : "vxqbtkxj2z37",
+            // "8ksrcjrcmpbq9rtvtvrbgljzqvbnxddnzt"  : "8ksrcjrcmpbq9rtvtvrbgljzqvbnxddnzt",
+            // mpftpsgp6fourvdmltwojd9               : "mpftpsgp64vdml2jd9",
+            // "2fivetwosix"                         : "2526",
+            // "3qqx2"                               : "3qqx2",
+            // jsbdh16snnllpvvgnggfive5nhjpgdzh      : "jsbdh16snnllpvvgngg55nhjpgdzh",
+            // "4fmgmmbonegtsnqfdqt1pm"              : "4fmgmmb1gtsnqfdqt1pm",
+            // threeighthree                         : "3igh3",
+            // four1six                              : "416",
+            // sdsevenine                            : "sd7ine"
             // 2onendvlmcrvzsnpr83nine
             // 8ninelfhkhnqtdfour
             // 8reight
@@ -107,16 +112,16 @@ describe("day 01", () => {
         });
     });
 
-    test.only("non-swapping", () => {
+    test("non-swapping", () => {
         [{
             input,
         }, {
-            input :     output
+            input : output
         }, {
-            input : sample01,
+            input    : sample01,
             expected : 142
         }].forEach(({ input : _input, expected }) => {
-            const result = sumFromRows(_input);
+            const result = sumRows(_input);
 
             console.log({ result });
 
@@ -128,22 +133,20 @@ describe("day 01", () => {
         });
     });
 
-    test.only("swapping", async () => {
-        let result = sumFromRows(swapMatchesAllRows(input));
+    test("swapping", async () => {
+        let result = swapAndSum(input);
 
         console.log({ result });
 
         expect(typeof result).toBe("number");
+        expect(result).toBeGreaterThan(54095);
 
-        // expect(result).toBeGreaterThan(54095);
 
-
-        result = sumFromRows(swapMatchesAllRows(sample02));
+        result = swapAndSum(sample02);
 
         console.log({ result });
 
         expect(typeof result).toBe("number");
-
         expect(result).toBe(281);
     });
 });
