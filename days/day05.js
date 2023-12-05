143c3aff1252ff53b879472c3f14dad41c55c525
Kevin Cameron <kevinacameron@hotmail.com>
2023-12-05 00:30:55 -0800

    d05p1 done

--- days/day05.js
+++ days/day05.js
@@ -1,20 +1,98 @@
-export function parse(str) {
-    return str.split(/\r?\n/);
-}
-
-export class Day05 {
+export class Almanac {
     constructor(str) {
         this.str = str;
-        this.lines = str.split(/\r?\n/);
+        this.maps = [];
 
-        this.parsed = this.parse();
+        this.parse();
     }
 
     parse() {
-        return this.lines.map(line => line.split(" "));
+        this.str.split(/(?:\r?\n){2}/).forEach((section, idx) => {
+            if (idx === 0) {
+                this.seeds = section.split(" ").filter((_, idx) => idx).map(Number);
+
+                return;
+            }
+
+            const map = {
+                name : "",
+                rows : []
+            };
+
+            section.split(/\r?\n/).forEach((row, idx) => {
+                if (idx === 0) {
+                    map.name = row;
+
+                    return;
+                }
+
+                map.rows.push(row.split(" ").map(Number));
+            });
+
+            this.maps.push(map);
+        });
+    }
+
+    _findMap(from, map) {
+        return Almanac._findMap(from, map);
+    }
+
+    _getDestFromMap(from, map) {
+        return Almanac._getDestFromMap(from, map);
+    }
+
+    _getDest(from, map) {
+        return Almanac._getDest(from, map);
+    }
+
+    _getFinalDestFromMaps(from, maps) {
+        return Almanac._getFinalDestFromMaps(from, maps);
+    }
+
+    static _findMap(from, maps) {
+        return maps.find((row, idx) => {
+            const [ dest, src, range ] = row;
+
+            return src <= from && from <= src + range;
+        });
+    }
+
+    static _getDestFromMap(from, map) {
+        return map[0] + (from - map[1]);
+    }
+
+    static _getDest(from, map) {
+        const foundMap = this._findMap(from, map);
+
+        return foundMap ? this._getDestFromMap(from, foundMap) : from;
+    }
+
+    static _getFinalDestFromMaps(from, maps) {
+        return maps.reduce((acc, map) => {
+            const dest = this._getDest(acc, map);
+
+            // console.log({ acc, map, dest });
+
+            return dest;
+        }, from);
+    }
+
+    getFinalDestForAllSeeds() {
+        return this.seeds.map((seed) => this._getFinalDestFromMaps(seed, this.maps.map((map) => map.rows)));
+    }
+
+    getMinFinalDestForAllSeeds() {
+        return Math.min(...this.getFinalDestForAllSeeds());
     }
 
     toString() {
-        return this.parsed.reduce((acc, line) => acc + line.join(" ") + "\n", "").trim();
+        const seedsStr = `seeds: ${this.seeds.join(" ")}`;
+        const maps = this.maps.map((map) => {
+            const rows = map.rows.map((row) => row.join(" ")).join("\r\n");
+
+            return `${map.name}\r\n${rows}`;
+        });
+
+        return [seedsStr, ...maps].join("\r\n\r\n");
     }
 }
--- test/05/day05.spec.js
+++ test/05/day05.spec.js
@@ -1,10 +1,10 @@
-import { readFile }      from "node:fs/promises";
+import { readFile, writeFile }      from "node:fs/promises";
 import { fileURLToPath } from "node:url";
 import path              from "node:path";
 
 import { expect, test, describe } from "vitest";
 
-import { Day05 } from "../../days/day05.js";
+import { Almanac } from "../../days/day05.js";
 
 import typedefs from "../../typedefs.js";
 
@@ -14,10 +14,75 @@ const __dirname  = path.dirname(__filename);
 const sample = await readFile(path.join(__dirname, "sample.txt"), "utf8");
 const input  = await readFile(path.join(__dirname, "input.txt"), "utf8");
 
+const map = [
+    [ 45, 10, 7 ],
+    [ 65, 45, 10 ],
+    [ 8, 24, 3 ],
+    [ 19, 80, 18 ],
+];
+const map2 = [
+    [ 45, 10, 7 ],
+    [ 65, 45, 10 ],
+    [ 65, 24, 3 ],
+    [ 65, 80, 18 ],
+];
+
 
 describe("day 05", () => {
-    test("can parse input", () => {
-        expect((new Day05(sample)).toString()).toBe(sample);
-        expect((new Day05(input)).toString()).toBe(input);
+    test("can parse input", async () => {
+
+        // await writeFile(path.join(__dirname, "sample.out.txt"), almanac.toString());
+
+        const sampleAlmanac = new Almanac(sample);
+
+        expect(sampleAlmanac.toString()).toBe(sample);
+
+
+        const almanac = new Almanac(input);
+
+        expect(almanac.toString()).toBe(input);
+    });
+
+    test("can find map", () => {
+
+        expect(Almanac._findMap(12, map)).toEqual(map[0]);
+        expect(Almanac._findMap(45, map)).toEqual(map[1]);
+        expect(Almanac._findMap(25, map)).toEqual(map[2]);
+        expect(Almanac._findMap(90, map)).toEqual(map[3]);
+        expect(Almanac._findMap(9, map)).toEqual();
+    });
+
+    test("can get dest from map", () => {
+        expect(Almanac._getDestFromMap(12, [ 45, 10, 7 ])).toBe(47);
+        expect(Almanac._getDestFromMap(12, [ 45, 12, 7 ])).toBe(45);
+        expect(Almanac._getDestFromMap(12, [ 45, 10, 2 ])).toBe(47);
+        expect(Almanac._getDestFromMap(12, [ 45, 10, 7 ])).toBe(47);
+    });
+
+    test("get dest", () => {
+        expect(Almanac._getDest(12, map)).toBe(47);
+        expect(Almanac._getDest(45, map)).toBe(65);
+        expect(Almanac._getDest(25, map)).toBe(9);
+        expect(Almanac._getDest(90, map)).toBe(29);
+        expect(Almanac._getDest(9, map)).toBe(9);
+        expect(Almanac._getDest(100, map)).toBe(100);
+    });
+
+    test("get final dest", () => {
+        expect(Almanac._getFinalDestFromMaps(12, [ map, map2 ])).toBe(67);
+    });
+
+    test("get final dest for all seeds", () => {
+        const almanac = new Almanac(sample);
+
+        expect(almanac.getFinalDestForAllSeeds()).toEqual([ 82, 43, 86, 35 ]);
+    });
+
+    test("answer p1", () => {
+        const almanac = new Almanac(input);
+
+        const minFinalDest = almanac.getMinFinalDestForAllSeeds();
+
+        expect(minFinalDest).toBe(107430936);
     });
 });
