19db230d43ca5bf07632cc1e96fd7d972784d9f0
Kevin Cameron <kevinacameron@hotmail.com>
2023-12-12 22:21:16 -0800

    d10 data

--- nextDay/template/test/samples.js
+++ nextDay/template/test/samples.js
@@ -1,5 +1,13 @@
-export const sample = ``;
-export const sample2 = ``;
+export const sample = `-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF`;
+export const sample2 = `7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ`;
 export const sample3 = ``;
 export const sample4 = ``;
 export const sample5 = ``;
